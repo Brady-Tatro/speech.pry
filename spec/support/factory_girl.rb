dfe322ee2fbe9185c367bc503cbe5fa7849534a9
Brady Tatro <bradytatro@gmail.com>
2016-10-19 17:49:12 -0400

    added speech tests. wip comment tests

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -6,4 +6,8 @@ FactoryGirl.define do
     sequence(:email) { |n| "partyeva#{n}@launchacademy.com" }
     sequence(:password) { |n| "123#{n}4567" }
   end
+  factory :speech do
+    sequence(:title) { |n| "speaking"}
+    sequence(:media) { "https://www.youtube.com/watch?v=FWnTJjAKwdY&index=28&list=PLKG--4mRoNpBn8WvKrLdNLkTxrqjx0hue" }
+  end
 end
