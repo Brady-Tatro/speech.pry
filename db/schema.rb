9072588a4a8202203c0248fde42a0143abfec42c
Brady Tatro <bradytatro@gmail.com>
2016-10-19 16:46:42 -0400

    added speech and comments to db. tests wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019193225) do
+ActiveRecord::Schema.define(version: 20161019204226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,11 +28,11 @@ ActiveRecord::Schema.define(version: 20161019193225) do
 
   create_table "speeches", force: :cascade do |t|
     t.string   "title",       null: false
+    t.string   "media",       null: false
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "user_id",     null: false
-    t.string   "media",       null: false
+    t.integer  "user_id"
     t.index ["user_id"], name: "index_speeches_on_user_id", using: :btree
   end
 
